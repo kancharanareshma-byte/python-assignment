0f06742ca254fc4dddeeb32d6a9018c4a2b7c67d
kancharanareshma-byte <kancharanareshma@gmail.com>
2026-03-12 22:13:13 +0530

    Update Student_academic_management_system.py
    A college wants to digitally manage student academic records, including marks, CGPA, age
    calculation, and fee balance. Create a class named Student with the following attributes:
    Student ID, Student Name, Marks in 3 subjects, Date of Birth, Fee Paid. Create methods to:
    calculate average marks and CGPA, calculate age of the student, calculate fee balance.
    Create another class named College with the following attributes: college code, college
    name, Location. Create methods to: register a student, display complete student academic
    details
    Assume a college registers 3 students and wants to generate a student academic report.
    Write a Python program to display: College details, Student details, CGPA, Age, Fee balance

--- Student_academic_management_system.py
+++ Student_academic_management_system.py
@@ -1,1 +1,28 @@
+class College:
+    def __init__(self, code, name, location, s1, s2, s3):
+        self.college_code = code
+        self.college_name = name
+        self.location = location
+        # Directly assigning the 3 student objects
+        self.s1 = s1
+        self.s2 = s2
+        self.s3 = s3
 
+    def display_report(self):
+        print("COLLEGE DETAILS")
+        print("Name:", self.college_name)
+        print("Code:", self.college_code)
+        print("Location:", self.location)
+        print("STUDENT ACADEMIC REPORT:")       
+        self.s1.print_studentdetails()
+        self.s2.print_studentdetails()
+        self.s3.print_studentdetails()
+# --- Execution ---
+# 1. Create three objects for Student class
+std1 = Student(81, "hasini", 85, 90, 88, 2006, 30000)
+std2 = Student(102, "Jashu", 70, 99, 98, 2006, 46000)
+std3 = Student(103, "Reshma", 77, 88, 99, 2006, 50000)
+# 2. Create one object for College class and pass the 3 students
+my_college = College("ANES001", "ANITS", "Visakhapatnam", std1, std2, std3)
+# 3. Display report
+my_college.display_report()
